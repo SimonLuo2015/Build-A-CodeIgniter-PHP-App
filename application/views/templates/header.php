0af8ab9537d578cfe582efdc8075210c7a094f5c
simonluo2015 <luoshujian1987@163.com>
2018-03-11 23:11:09 +0800

    和教材不太一样，增加了导航栏的显示，还需要调整一下。

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,5 +1,23 @@
 <html>
     <head>
         <title>ciBlog</title>
+        <link rel="stylesheet" href="https://bootswatch.com/4/flatly/bootstrap.min.css">
     </head>
     <body>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand" href="#">ciBlog</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation" style="">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarColor02">
+            <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="<?php echo base_url(); ?>">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="<?php echo base_url().'about'; ?>">About</a>
+            </li>
+            </ul>
+        </div>
+        </nav>
